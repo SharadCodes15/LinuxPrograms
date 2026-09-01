86b4c933c41264a13683b8b5615746fa24b346ee
SharadCodes15 <codebysharad@gmail.com>
2026-09-01 19:49:29 +0530

    [TODO] -> Add Delete Function

--- Project/ToDo/app.py
+++ Project/ToDo/app.py
@@ -1,9 +1,10 @@
+import json
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal
 from textual.widgets import ListView, Static, ListItem, Label, Footer
 
 from sidebar import Sidebar, SidebarItem
-from content import Content
+from content import Content, ContentItem
 
 
 class Todo(App):
@@ -32,6 +33,36 @@ class Todo(App):
 
             content = self.query_one(Content)
             content.show_category(category)
+
+    def action_delete(self) -> None:
+        """Delete the selected todo item"""
+        task_list = self.query_one("#task-list", ListView)
+        selected_index = task_list.index
+        
+        if selected_index is None:
+            return
+        
+        # Get the selected item
+        selected_item = task_list.children[selected_index]
+        
+        if isinstance(selected_item, ContentItem):
+            # Load the todo data
+            with open("todo.json", "r", encoding="utf-8") as data:
+                todo_data = json.load(data)
+            
+            # Remove the todo from the data
+            task_text = selected_item.task_text
+            todo_data["todos"] = [
+                todo for todo in todo_data["todos"]
+                if todo["title"] != task_text
+            ]
+            
+            # Save back to file
+            with open("todo.json", "w", encoding="utf-8") as data:
+                json.dump(todo_data, data, indent=2)
+            
+            # Remove from UI
+            task_list.remove(selected_item)
 
 
 
